7199244888913759b940c3b7a6805cb39c982545
Szabolcs Toth <tsz@purzelbaum.hu>
2020-04-16 22:55:08 +0200

    v0.0.3

--- lib/oci_config_reader.rb
+++ lib/oci_config_reader.rb
@@ -16,7 +16,7 @@ module OCR
       if line !=~ /[DEFAULT]/
         oci_config_data["user"] = "#{line.match(/(?<==).+/)}" if line =~ /user/
         oci_config_data["fingerprint"] = "#{line.match(/(?<==).+/)}" if line =~ /fingerprint/
-        oci_config_data["key_file"] = "#{line.match(/(?<==).+/)}" if line =~ /key_file/
+        oci_config_data["key_file"] = ("#{line.match(/(?<==).+/)}").gsub(/~/, "#{Dir.home}") if line =~ /key_file/
         oci_config_data["tenancy"] = "#{line.match(/(?<==).+/)}" if line =~ /tenancy/
         oci_config_data["region"] = "#{line.match(/(?<==).+/)}" if line =~ /region/
       end
--- lib/oci_config_reader/version.rb
+++ lib/oci_config_reader/version.rb
@@ -1,3 +1,3 @@
 module OCR
-  VERSION = "0.0.2"
+  VERSION = "0.0.3"
 end
